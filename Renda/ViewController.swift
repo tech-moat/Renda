8e1aff4415910090661a59c0fbf959319d5b0522
HiromasaHori <horihiromasa.tech@gmail.com>
2022-08-27 15:44:15 +0900

    firebase完了

--- Renda/ViewController.swift
+++ Renda/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
     
@@ -16,17 +17,43 @@ class ViewController: UIViewController {
     var tapCount = 0
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        tapButton.layer.cornerRadius = 125
-    }
+            super.viewDidLoad()
+            // Do any additional setup after loading the view.
+            // ボタンを丸くする
+            tapButton.layer.cornerRadius = 125
+
+            // ここから下を追加
+            // Firestoreのデータを読み込む
+            firestore.collection("counts").document("share").addSnapshotListener { snapshot, error in
+                if error != nil {
+                    print("エラーが発生しました")
+                    print(error)
+                    return
+                }
+                let data = snapshot?.data()
+                if data == nil {
+                    print("データがありません")
+                    return
+                }
+                let count = data!["count"] as? Int
+                if count == nil {
+                    print("countという対応する値がありません")
+                    return
+                }
+                self.tapCount = count!
+                self.countLabel.text = String(count!)
+            }
+        }
+    
+    let firestore = Firestore.firestore()
 
     @IBAction func tapTapButton(){
         
         tapCount += 1
         
         countLabel.text = String(tapCount)
+        
+        firestore.collection("counts").document("share").setData(["count": tapCount])
     }
     
 }
